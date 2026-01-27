e3367c29fd10198b1b76683563504a4c1e53b438
keanutoofa <keanu.toofa@gmail.com>
2026-01-27 16:33:01 +0100

    Draw the top-right quadrant

--- src/hands_on_monte_carlo_simulation/main.py
+++ src/hands_on_monte_carlo_simulation/main.py
@@ -1,2 +1,46 @@
+import pygame
+from .parser import parse_arguments
+from .quadrant_circle import draw_quadrant_circle
+
 def main() -> None:
-    print("Hello from hands-on-monte-carlo-simulation!")
+    args = parse_arguments()
+    print(f"Parsed arguments: {args}")
+
+    #on vérifie si on active le mode GUI avec l'argument -x
+    if args.gui:
+        print("Initializing GUI...")
+        pygame.init()
+
+        #Création de la fenêtre
+        screen = pygame.display.set_mode((args.width, args.height))
+        pygame.display.set_caption("Monte Carlo Simulation")
+
+        #Configuration des couleurs et variables
+        bg_color = args.bg_color
+        circle_color = args.circle_color
+        line_width = args.line_width
+        running = True
+
+        
+        while running:
+           
+            for event in pygame.event.get():
+
+                #Fermer la fenêtre
+                if event.type == pygame.QUIT:
+                    running = False
+            
+
+            screen.fill(bg_color)                 
+            draw_quadrant_circle(screen, circle_color, line_width) 
+            
+            # Rafraîchissement de l'écran
+            pygame.display.flip()
+
+        # Fermeture de Pygame
+        pygame.quit()
+
+    else:
+        print("Hello from hands-on-monte-carlo-simulation!")
+        print("Mode CLI activé (pas de fenêtre graphique). Utilisez -x pour le GUI.")
+
