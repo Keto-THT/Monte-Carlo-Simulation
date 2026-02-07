b8d35ebe06cd001973d659f0a6722cb7b6cdbdb9
keanutoofa <keanu.toofa@gmail.com>
2026-02-07 17:02:35 +0100

    Add PointGenerator Class

--- src/hands_on_monte_carlo_simulation/monte-carlo_simulation.py
+++ src/hands_on_monte_carlo_simulation/monte-carlo_simulation.py
@@ -15,6 +15,7 @@ class MonteCarloSimulation:
         Args:
             seed (int, optional): La graine pour le générateur de nombres aléatoires. Defaults to None.
         """
+        self.seed = seed
         if seed is not None:
             random.seed(seed)
         
@@ -42,7 +43,7 @@ class MonteCarloSimulation:
         """
         return (x * x + y * y) <= 1
     
-    def generate_point(self, n: int) -> Tuple[List[Tuple[float, float]], int]:
+    def generate_point(self, n: int) -> Tuple[List[Tuple[float, float, bool]], int]:
         """
         Génère n points aléatoires dans le carré de côté 1 et compte ceux qui sont dans le quadrant
 
@@ -64,7 +65,7 @@ class MonteCarloSimulation:
             if is_inside:
                 count_inside += 1
 
-        return points, count_inside
+        return (points, count_inside)
     
     @staticmethod
     def calculate_pi(total_points: int, count_inside: int,) -> float:
@@ -84,5 +85,52 @@ class MonteCarloSimulation:
         ratio = count_inside / total_points
         return 4.0 * ratio
 
+class PointGenerator : 
+    """
+    Classe pour générer les points en utilisant le threading
+    """
 
+    def __init__(self, seed: int = None):
+        """
+        Initialise le générateur
+
+        Args : 
+            seed (int, optional): La graine pour le générateur de nombres aléatoires. Defaults to None.
+        """
+        if seed is not None:
+            random.seed(seed)
+        
+        self.calculator = MonteCarloSimulation(seed)
+    
+    def generate_batch(self, n: int) -> Tuple[int, int]:
+        """
+        Génère un échantillon de n points et retourne les statistiques
+
+        Args:
+            n (int): Nombre de point à générer
+        
+        Returns:
+            Tuple de la forme (total_points, inside_points)
+        """
+
+        points, inside_points = self.calculator.generate_point(n)
+
+        return (n, inside_points)
+    
+    def generate_points_with_details(self, n: int) -> Tuple[List[Tuple[float, float, bool]], int]:
+        """
+        Génère un échantillon de n points et retourne les détails de chaque point pour l'affichage GUI
+
+        Args:
+            n (int): Nombre de point à générer
+        
+        Returns:
+            Tuple de la forme ([(x1, y1, is_inside), ...], inside_points)
+        """
+        return self.calculator.generate_point(n)
+
+        
+
+
+    
     
