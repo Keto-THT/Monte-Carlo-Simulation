fbfc40613353b8985db223e4d14caf9b1ce9b904
keanutoofa <keanu.toofa@gmail.com>
2026-02-07 17:38:02 +0100

    Add MonteCarloThread class

--- src/hands_on_monte_carlo_simulation/threading_manager.py
+++ src/hands_on_monte_carlo_simulation/threading_manager.py
@@ -23,5 +23,58 @@ class ThreadResult:
         self.total_points += len(points)
         self.inside_points += sum(1 for _, _, is_inside in points if is_inside)
 
+class MonteCarloThread(threading.Thread):
+    """
+    Classe de threading pour exécuter la simulation de Monte Carlo en parallèle
+    """
 
+    def __init__(self, thread_id: int, nb_draws: int, seed: int = None, callback: callable = None, result_container: ThreadingResult = None):
+        """
+        Initialise le thread
 
+        Args:
+            thread_id (int): L'identifiant du thread
+            nb_draws (int): Le nombre de tirages à effectuer dans ce thread
+            seed (int, optional): La graine pour le générateur de nombres aléatoires. Defaults to None.
+            callback (callable, optional): Une fonction de rappel à appeler avec les résultats du thread pour l'affichage GUI. Defaults to None.
+            result_container (ThreadResult, optional): Un conteneur pour stocker les résultats du thread. Defaults to None.
+        """
+        super().__init__()
+        self.thread_id = thread_id
+        self.nb_draws = nb_draws
+        self.seed = seed
+        self.callback = callback
+        self.result_container = result_container
+
+        #génération d'une graine unique pour ce thread
+        if seed is not None:
+            self.seed = seed + thread_id
+        else:
+            self.seed = None
+        
+        self.generator = PointGenerator(self.seed)
+
+        def run(self):
+            """
+            Exécute la génération de points pour ce thread et stocke les résultats 
+            """
+
+            # Mode GUI : génère les points un par un et utilise le callback pour l'affichage
+            if self.callback is not None:
+                for _ in range(self.nb_draws):
+                    points, inside = self.generator.generate_points_with_details(1)  # Génère un point à la fois pour le GUI
+                    if points : 
+                        x, y, is_inside = points[0]
+                        self.callback(x, y, is_inside)
+
+                        if self.result_container is not None:
+                            self.result_container.total_points += 1
+                            if is_inside: 
+                                self.result_container.inside_points += 1
+            
+            # Mode non-GUI : génère tous les points en batch en une fois et stocke les résultats dans le conteneur
+            else:
+                points, inside = self.generator.generate_points_with_details(self.nb_draws)
+                
+                if self.result_container is not None:
+                    self.result_container.add_points_data(points)
